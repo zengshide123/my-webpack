374216c1dd531af8de0bb73d27064770c278ee14
liu lang yi ge <1909493019@qq.com>
2019-06-10 08:01:23 +0800

    add-MPA

---
+++ demo2/readme.md
@@ -0,0 +1,18 @@
+## MPA   
+     多页应用
+###  特别使用到的插件
+     - 使用html-webpack-plugin时,一次实例生成一个页面.使用chunks来指定文件依赖入口
+###  实现功能
+
+> 注意: 本版主要为单页,简单实现的功能,针对development
+
+     - es6转es5
+     - less scss postcss预处理
+     - 图片 font加载
+     - 提取css文件
+     - 模块热更新
+     - source-map
+
+
+
+    
---
+++ demo2/webpack.config.js
@@ -0,0 +1,143 @@
+let path = require('path');
+let webpack = require('webpack');
+let { CleanWebpackPlugin } = require('clean-webpack-plugin');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry:{
+        "index":'./src/index.js',
+        "anthor":"./src/other.js"
+    },
+    mode:"development",
+    output:{
+        filename:'./js/[name][hash:8].js',
+        path:path.resolve(__dirname,'dist'),
+        // publicPath: 'https://cdn.example.com/assets/'
+    },
+    devtool:"source-map",
+    devServer:{
+        contentBase:"./dist",
+        port:3000,
+        open:true,
+        progress:true,
+        hot:true
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:[
+                    {
+                        loader:MiniCssExtractPlugin.loader,
+                        options:{
+                            insertAt:'top'
+                        }
+                    },
+                    {
+                        loader:'css-loader'
+                    },
+                    {
+                        loader:'postcss-loader'
+                    }
+                ]
+            },
+            {
+                test:/\.less$/,
+                use:[
+                    {
+                        loader:MiniCssExtractPlugin.loader,
+                        options:{
+                            insertAt:'top'
+                        }
+                    },
+                    {
+                        loader:'css-loader'
+                    },
+                    {
+                        loader:'postcss-loader'
+                    },
+                    {
+                        loader:'less-loader'
+                    }
+                ]
+            },
+            {
+                test:/\.scss$/,
+                use:[
+                    {
+                        loader:MiniCssExtractPlugin.loader,
+                        options:{
+                            insertAt:'top'
+                        }
+                    },
+                    {
+                        loader:'css-loader'
+                    },
+                    {
+                        loader:'postcss-loader'
+                    },
+                    {
+                        loader:'sass-loader'
+                    }
+                ]
+            },
+            {
+               test: /.js$/,
+               use: {
+                   loader: "babel-loader"
+               }
+           },
+           {
+            test: /\.(png|svg|jpg|gif)$/,
+            use:[
+                {
+                    loader:"file-loader",
+                    options: {
+                        outputPath: 'images',
+                      }
+                }
+            ]                   
+           },
+           {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use:[
+                {
+                  loader:"file-loader",
+                  options: {
+                    outputPath: 'css/font',
+                  }
+                }
+            ]
+           },
+           {
+            test: /\.(html)$/,
+            use: {
+              loader: 'html-loader'
+            }
+          }
+        ]
+    },
+    // externals: {
+    //     jquery: '$'
+    // },
+    plugins:[
+        new webpack.ProgressPlugin(),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template:'./index.html',
+            filename:"index.html",
+            hash:true,
+            chunks:['index']
+        }),
+        new HtmlWebpackPlugin({
+            template:'./index.html',
+            filename:"anthor.html",
+            hash:true,
+            chunks:['anthor']
+        }),
+        new MiniCssExtractPlugin({
+            filename:'./css/[name].css'
+        })
+    ]
+}
